13ba01f393e3543c91603e4a2a67e0f2db49262e
Michal Pieszczyk <michalpieszczyk@gmail.com>
2019-05-09 21:14:50 +0200

    Updated home screen and basic funcionality of the camera

--- app/src/main/java/com/example/intercomandroid/MainActivity.java
+++ app/src/main/java/com/example/intercomandroid/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.intercomandroid;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView MyFaceHomeScreen;
+    private Button ButtonCall;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyFaceHomeScreen = (ImageView)findViewById(R.id.MyFaceHomeScreen);
+        ButtonCall = (Button)findViewById(R.id.ButtonCall);
+
+        ButtonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+
+            }
+        });
     }
+
+    private void takePicture() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 0);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK)
+        {
+            Bitmap b = (Bitmap)data.getExtras().get("data");
+            MyFaceHomeScreen.setImageBitmap(b);
+        }
+    }
+
 }
