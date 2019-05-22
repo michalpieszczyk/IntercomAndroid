0f4a4c9ed6320ebe1f0539a755657a4f26796d98
Michal Pieszczyk <michalpieszczyk@gmail.com>
2019-05-22 20:24:19 +0200

    Fixed home screen layout, successful Firebase database integration. Currently working on uploading photos to Firebase storage.

--- app/src/main/java/com/example/intercomandroid/MainActivity.java
+++ app/src/main/java/com/example/intercomandroid/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.intercomandroid;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -26,15 +36,18 @@ public class MainActivity extends AppCompatActivity {
     private ImageView MyFaceHomeScreen;
     private Button ButtonCall;
     private Button ButtonSendData;
+    public Uri file;
+    public Bitmap b;
     Member member;
     DatabaseReference reff;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(MainActivity.this);
-
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
 
 
         MyFaceHomeScreen = (ImageView)findViewById(R.id.MyFaceHomeScreen);
@@ -46,9 +59,24 @@ public class MainActivity extends AppCompatActivity {
         ButtonSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                member.setDate(DateFormat.getDateTimeInstance().format(new Date()));
-                reff.push().setValue(member);
-                Toast.makeText(MainActivity.this,"Succesfuly added data to firebase!",Toast.LENGTH_LONG).show();
+                Uri file = getImageUri(null,b);
+                StorageReference Ref = mStorageRef.child(member.getDate()+'.'+getExtension(file));
+
+                Ref.putFile(file)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // Get a URL to the uploaded content
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                                // ...
+                            }
+                        });
 
             }
         });
@@ -58,7 +86,9 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v) {
               takePicture();
 
-            }
+
+
+           }
        });
     }
 
@@ -72,10 +102,28 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK)
         {
-            Bitmap b = (Bitmap)data.getExtras().get("data");
-            //member.setImage(b);
+            b = (Bitmap)data.getExtras().get("data");
+            member.setImage(b);
+            member.setDate(DateFormat.getDateTimeInstance().format(new Date()));
             MyFaceHomeScreen.setImageBitmap(b);
+            reff.push().setValue(member.getDate());
+            Toast.makeText(MainActivity.this,"Succesfuly added data to firebase!",Toast.LENGTH_LONG).show();
+
         }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private String getExtension(Uri uri)
+    {
+        ContentResolver cr=getContentResolver();
+        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
     }
 
 }
