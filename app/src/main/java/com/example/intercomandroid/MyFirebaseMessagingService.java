3c8028b0c2a26c7a1845002d473bfcc6ba0e123e
Michal Pieszczyk <michalpieszczyk@gmail.com>
2019-05-26 10:28:08 +0200

    Updated notifications (Receive notification which was send from firebase console)

---
+++ app/src/main/java/com/example/intercomandroid/MyFirebaseMessagingService.java
@@ -0,0 +1,63 @@
+package com.example.intercomandroid;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        sendNotification(remoteMessage.getNotification().getBody());
+
+    }
+
+    private void sendNotification(String body) {
+
+        Intent intent = new Intent (this,MainActivity.class);
+        intent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setSmallIcon(R.drawable.ic_stat_name);
+        notificationBuilder.setContentTitle("Someone is comming!");
+        notificationBuilder.setContentText(body);
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSound(defaultSoundUri);
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+
+    }
+
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+    }
+}
